1d7afc1e46f7307ab60c086f30136f12b580defd
Хімін Роман <125272724+karasu000@users.noreply.github.com>
2023-09-28 14:26:47 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(x);
+                if (x == 0)
+                {
+                    x = 1;
+                }
+                else
+                {
+                    int z = x;
+                    x = x + y;
+                    y = z;
+                }
+           }
+            Console.ReadLine();
+        }
+
+    }
+}
